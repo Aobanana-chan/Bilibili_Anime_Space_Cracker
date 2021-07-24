da9406d56978fd12fd436682bf52080dd9bb913b
青葉ナナ <940610311@qq.com>
2021-07-24 14:53:32 +0800

    commit

---
+++ node_modules/ajax-hook/index.js
@@ -0,0 +1,8 @@
+/*
+ * author: wendux
+ * email: 824783146@qq.com
+ * source code: https://github.com/wendux/Ajax-hook
+ **/
+export {hook, unHook}from "./src/xhr-hook"
+export {proxy, unProxy} from "./src/xhr-proxy"
+
---
+++ node_modules/ajax-hook/src/cdn-core.js
@@ -0,0 +1,12 @@
+/*
+ * author: wendux
+ * email: 824783146@qq.com
+ * source code: https://github.com/wendux/Ajax-hook
+ */
+
+import {hook, unHook} from "./xhr-hook";
+
+export var ah = {
+    hook,
+    unHook,
+};
